e3b56f41305236362734835d6863c154248a20b0
ValmikGiri <valmikgiri@gmail.com>
2016-08-24 15:03:08 +0530

    Update ReadFromExcel.cs

--- ReadFromExcel.cs
+++ ReadFromExcel.cs
@@ -14,12 +14,13 @@ namespace AdvancedFindAutomation.TestData
             string strExcelFile = @"E:\AdvancedFind\AdvancedFindAutomation\AdvancedFindAutomation\TestData\TestData.xlsx";
             var connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + strExcelFile + @";Extended Properties=""Excel 12.0;IMEX=1;HDR=NO;TypeGuessRows=0;ImportMixedTypes=Text""";
 
-            OleDbDataAdapter Adapter = new OleDbDataAdapter();
+            OleDbDataAdapter Adapter = new OleDbDataAdapter();//Sealed Class- Set of data Command that are used to fill dataset & update data source during db connection.
             OleDbConnection conn = new OleDbConnection(connectionString);
+            //Connection Db- Sealed class
             conn.Open();
 
             DataTable ExcelSheets = conn.GetOleDbSchemaTable(System.Data.OleDb.OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
-
+//Data Tables are responsible to get DB schema table data
             switch (sheetName)
             {
                 case "Browser":
@@ -28,11 +29,13 @@ namespace AdvancedFindAutomation.TestData
                 case "AdvancedFind":
                     strQuery = "select * from [" + sheetName + "$A" + rowId + ":G" + rowId + "]";
                     break;
+                    //Select Sheet and its range
             }
 
             OleDbCommand cmd = new OleDbCommand(strQuery, conn);
+            // Represents an SQL statement or stored procedure to execute against a data source.
             Adapter.SelectCommand = cmd;
-            DataSet dsExcel = new DataSet();
+            DataSet dsExcel = new DataSet(); //Represents an in-memory cache of data.
 
             Adapter.Fill(dsExcel);
 
